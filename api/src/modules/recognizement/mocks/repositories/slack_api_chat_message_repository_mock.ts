23814f32195a623e1e0d7b5974dcef381c1d8174
Guilherme Isaías <guilherme.isaias@ferreri.co>
2024-07-21 15:21:28 -0300

    slack api repository mock was updated

--- api/src/modules/recognizement/mocks/repositories/slack_api_chat_message_repository_mock.ts
+++ api/src/modules/recognizement/mocks/repositories/slack_api_chat_message_repository_mock.ts
@@ -1,5 +1,6 @@
+import '@types/jest'
 import { SlackApiChatMessageRepository } from "../../@types/repositories/slack_api_chat_message_repository";
 
 export class SlackApiChatMessageRepositoryMock implements SlackApiChatMessageRepository {
-  
+  create = jest.fn()
 }
