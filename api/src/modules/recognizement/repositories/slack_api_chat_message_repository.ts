dc7e032b22f8b059e3c12e7fde2b3181ead2eed2
Guilherme Isaías <guilherme.isaias@ferreri.co>
2024-07-21 18:25:10 -0300

    dynamic slack api obtetion was added to slack api repository

--- api/src/modules/recognizement/repositories/slack_api_chat_message_repository.ts
+++ api/src/modules/recognizement/repositories/slack_api_chat_message_repository.ts
@@ -1,20 +1,20 @@
 import { WebClient } from "@slack/web-api";
-import { Recognizement } from "../@types/entities";
+import { RecognizementInput } from "../@types/entities";
 import { SlackApiChatMessageRepository } from "../@types/repositories/slack_api_chat_message_repository";
 import { SlackMessageCreationError } from "../../../errors/slack_message_creation_error";
 
 export class SlackApiChatMessageRepositoryImpl implements SlackApiChatMessageRepository {
-  private readonly slack: WebClient;
 
-  constructor(slack_client: WebClient) {
-    this.slack = slack_client;
-  }
-
-  async create(payload: Recognizement): Promise<void> {
-    const message = await this.slack.chat.postMessage({
+  async create(payload: RecognizementInput): Promise<void> {
+    const slack = await this.get_slack_client(payload.slack_integration_key);
+    const message = await slack.chat.postMessage({
       text: payload.message,
       channel: payload.slack_team_member_id
     })
     if(!message.ok) throw new SlackMessageCreationError(message.error);
+  }
+
+  private async get_slack_client(integration_key: string) {
+    return new WebClient(integration_key);
   }
 }
