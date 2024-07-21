fac9b6f9fcb2dfeade3ccffab53887bf04cfca72
Guilherme Isaías <guilherme.isaias@ferreri.co>
2024-07-21 20:33:38 -0300

    create recognizemente tes was updated

--- api/src/modules/slack_team/service.test.ts
+++ api/src/modules/slack_team/service.test.ts
@@ -1,29 +1,41 @@
-import { describe, expect, test } from '@jest/globals';
+import { beforeEach, describe, expect, test } from '@jest/globals';
 import { SlackTeamService } from './@types/service';
-import { beforeEach } from 'node:test';
 import { SlackTeamServiceImpl } from './service';
 import { RecognizementServiceMock } from '../recognizement/mocks/service';
 import { SlackTeamRepositoryMock } from './mocks/repositories/slack_team_repository_mock';
 
 describe('SlackTeamService', () => {
   let service: SlackTeamService;
+  let recognizements: RecognizementServiceMock;
+  let slack_team_repository: SlackTeamRepositoryMock;
 
   beforeEach(async () => {
-    const recognizements = new RecognizementServiceMock()
-    const slack_team_repository = new SlackTeamRepositoryMock()
+    recognizements = new RecognizementServiceMock()
+    slack_team_repository = new SlackTeamRepositoryMock()
     service = new SlackTeamServiceImpl(recognizements, slack_team_repository)
   })
 
   describe('.create_recognizement', () => {
     test('when success', async () => {
-      const recognizement = await service.create_recognizement({
+      const input = {
         message: 'test message',
         slack_team_id: '55',
         slack_team_member_id: '55'
+      }
+      recognizements.create.mockResolvedValue({
+        message: input.message,
+        slack_team_member_id: input.slack_team_member_id,
       })
-      expect(recognizement.message).toBe('test message');
-      expect(recognizement.slack_team_member_id).toBe('55')
+      slack_team_repository.get.mockResolvedValue({
+        integration_key: 'integration key test',
+        name: 'test',
+        slack_id: input.slack_team_id,
+      })
+      const recognizement = await service.create_recognizement(input)
+      expect(recognizement.message).toBe(input.message);
+      expect(recognizement.slack_team_member_id).toBe(input.slack_team_member_id)
       expect(recognizement).not.toHaveProperty('slack_integration_key')
+      expect(recognizement).not.toHaveProperty('slack_team_id')
     })
   })
 })
