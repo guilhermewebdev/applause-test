ce867a6b77cdc98d899345499bef577d31038e24
Guilherme Isaías <guilherme.isaias@ferreri.co>
2024-07-21 20:51:52 -0300

    refactoring

--- api/src/modules/slack_team/policy.ts
+++ api/src/modules/slack_team/policy.ts
@@ -1,7 +1,3 @@
-import { RequestHandler } from "express";
-import { ParsedQs } from "qs";
-import { RecognizementOutput } from "../recognizement";
-import { RecognizementCreationInput, SlackTeamInput, SlackTeamOutput } from "./@types/entities";
 import { SlackTeamPolicy } from "./@types/policy";
 
 export class SlackTeamPolicyImpl implements SlackTeamPolicy {
