fd068bfd61a4973c452de9affc13b44b6bb02920
Guilherme Isaías <guilherme.isaias@ferreri.co>
2024-07-21 00:57:35 -0300

    slack team controller input and output were changed

--- api/src/modules/slack_team/@types/controller.d.ts
+++ api/src/modules/slack_team/@types/controller.d.ts
@@ -2,7 +2,15 @@ import { RequestHandler } from "express";
 import { SlackTeam, SlackTeamInput, SlackTeamOutput } from "./entities";
 
 export interface SlackTeamController {
-  create: RequestHandler<{}, SlackTeamInput, SlackTeamOutput>;
-  list: RequestHandler<{}, void, SlackTeamOutput[]>;
+  create: RequestHandler<
+    void,
+    { slack_team: SlackTeamInput },
+    { slack_team: SlackTeamOutput }
+  >;
+  list: RequestHandler<
+    {},
+    void,
+    { slack_teams: SlackTeamOutput[] }
+  >;
   delete: RequestHandler<{}, void, void>;
 }
