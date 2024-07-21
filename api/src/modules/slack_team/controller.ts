9759b89b6be1dd48cbbf841e7c35c17a7db33ef8
Guilherme Isaías <guilherme.isaias@ferreri.co>
2024-07-21 20:51:39 -0300

    empty slack team controller methods were created

---
+++ api/src/modules/slack_team/controller.ts
@@ -0,0 +1,19 @@
+import { SlackTeamController } from "./@types/controller";
+
+export class SlackTeamControllerImpl implements SlackTeamController {
+  create_recognizement = async () => {
+
+  }
+
+  create = async () => {
+
+  }
+
+  delete = async () => {
+
+  }
+
+  list = async () => {
+
+  }
+}
