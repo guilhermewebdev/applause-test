64c01c9b91f513188d9137ea84ae7b8e9d8182a4
Guilherme Isaías <guilherme.isaias@ferreri.co>
2024-07-21 01:05:19 -0300

    slack team member controller was created

---
+++ api/src/modules/slack_team_member/@types/controller.d.ts
@@ -0,0 +1,14 @@
+import { RequestHandler } from "express";
+import { SlackTeamMemberOutput } from "./entities";
+
+export interface SlackTeamMemberController {
+  list: RequestHandler<
+    { slack_id: string },
+    void,
+    {
+      slack_team_members: SlackTeamMemberOutput[],
+      next_page_cursor: string;
+    },
+    { page_cursor?: string }
+  >
+}
