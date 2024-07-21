fba6590337ce77939c80d833ecbdaeb412dcca2c
Guilherme Isaías <guilherme.isaias@ferreri.co>
2024-07-21 20:48:55 -0300

    incomplete slack team policy methods were created

---
+++ api/src/modules/slack_team/policy.ts
@@ -0,0 +1,23 @@
+import { RequestHandler } from "express";
+import { ParsedQs } from "qs";
+import { RecognizementOutput } from "../recognizement";
+import { RecognizementCreationInput, SlackTeamInput, SlackTeamOutput } from "./@types/entities";
+import { SlackTeamPolicy } from "./@types/policy";
+
+export class SlackTeamPolicyImpl implements SlackTeamPolicy {
+  create_recognizement = async () => {
+
+  }
+
+  create = async () => {
+
+  }
+
+  delete = async () => {
+
+  }
+
+  list = async () => {
+
+  }
+}
