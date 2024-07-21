2f7b302863bcc06675870e62565dbe9b5d7e6ade
Guilherme Isaías <guilherme.isaias@ferreri.co>
2024-07-21 18:24:21 -0300

    Recognizement module was created

--- api/src/modules/recognizement/index.ts
+++ api/src/modules/recognizement/index.ts
@@ -1,5 +1,27 @@
+import { Db } from 'mongodb'
+import { RecognizementModule } from './@types/module'
+import { RecognizementService } from './@types/service'
+import { RecognizementRepositoryImpl } from './repositories/recognizement_repository'
+import { SlackApiChatMessageRepositoryImpl } from './repositories/slack_api_chat_message_repository'
+import { RecognizementServiceImpl } from './service'
+import { Recognizement } from './@types/entities'
+
 export * from './@types/entities'
 export * from './@types/repositories/recognizement_repository'
 export * from './@types/repositories/slack_api_chat_message_repository'
 export * from './@types/service'
 export * from './@types/module'
+
+export class RecognizementModuleImpl implements RecognizementModule {
+  readonly service: RecognizementService;
+
+  constructor(db: Db) {
+    const db_collection = db.collection<Recognizement>('notifications');
+    const recognizement_repository = new RecognizementRepositoryImpl(db_collection);
+    const slack_api_chat_message_repository = new SlackApiChatMessageRepositoryImpl();
+    this.service = new RecognizementServiceImpl(
+      recognizement_repository,
+      slack_api_chat_message_repository
+    )
+  }
+}
