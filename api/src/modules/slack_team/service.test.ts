31fc73effea50c9b176e9d67c36065311a2e9f64
Guilherme Isaías <guilherme.isaias@ferreri.co>
2024-07-23 00:05:32 -0300

    tests were created to slack team service creation

--- api/src/modules/slack_team/service.test.ts
+++ api/src/modules/slack_team/service.test.ts
@@ -3,16 +3,25 @@ import { SlackTeamService } from './@types/service';
 import { SlackTeamServiceImpl } from './service';
 import { RecognizementServiceMock } from '../recognizement/mocks/service';
 import { SlackTeamRepositoryMock } from './mocks/repositories/slack_team_repository_mock';
+import { SlackApiTeamRepository } from './@types/repositories/slack_api_team_repository';
+import { SlackApiTeamRepositoryMock } from './mocks/repositories/slack_api_team_repository_mock';
+import { SlackTeam } from './@types/entities';
 
 describe('SlackTeamService', () => {
   let service: SlackTeamService;
   let recognizements: RecognizementServiceMock;
   let slack_team_repository: SlackTeamRepositoryMock;
+  let slack_api_team_repository: SlackApiTeamRepositoryMock;
 
   beforeEach(async () => {
     recognizements = new RecognizementServiceMock()
     slack_team_repository = new SlackTeamRepositoryMock()
-    service = new SlackTeamServiceImpl(recognizements, slack_team_repository)
+    slack_api_team_repository = new SlackApiTeamRepositoryMock()
+    service = new SlackTeamServiceImpl(
+      recognizements,
+      slack_team_repository,
+      slack_api_team_repository
+    )
   })
 
   describe('.create_recognizement', () => {
@@ -37,6 +46,26 @@ describe('SlackTeamService', () => {
       expect(recognizement.slack_team_member_id).toBe(input.slack_team_member_id)
       expect(recognizement).not.toHaveProperty('slack_integration_key')
       expect(recognizement).not.toHaveProperty('slack_team_id')
+    })
+
+  })
+
+  describe('.create', () => {
+
+    test('when success', async () => {
+      const slack_team_mock: SlackTeam = {
+        integration_key: 'test',
+        name: 'slack team name',
+        slack_id: '55'
+      }
+      slack_api_team_repository.get.mockResolvedValue(slack_team_mock)
+      slack_team_repository.create.mockResolvedValue(slack_team_mock);
+      const created = await service.create({
+        integration_key: 'test'
+      });
+      expect(created).toEqual(slack_team_mock);
+      expect(slack_api_team_repository.get).lastCalledWith(slack_team_mock.integration_key);
+      expect(slack_team_repository.create).toBeCalledWith(slack_team_mock);
     })
 
   })
