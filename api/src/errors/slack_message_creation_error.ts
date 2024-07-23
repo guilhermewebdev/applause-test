d07dfcfb1f38a72d36822c8bea8f34368347b3e6
Guilherme Isaías <guilherme.isaias@ferreri.co>
2024-07-22 22:52:35 -0300

    error type was fixed

--- api/src/errors/slack_message_creation_error.ts
+++ api/src/errors/slack_message_creation_error.ts
@@ -1,9 +1,9 @@
 export class SlackMessageCreationError {
   readonly code = 500;
   readonly message = 'Failed to send message';
-  readonly caused_by: string;
+  readonly caused_by?: string;
 
-  constructor(caused_by: string) {
+  constructor(caused_by?: string) {
     this.caused_by = caused_by;
   }
 }
