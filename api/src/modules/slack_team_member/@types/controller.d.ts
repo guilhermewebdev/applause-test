c177f586621e0bddb1eca91e9047a21aacdd22f4
Guilherme Isaías <guilherme.isaias@ferreri.co>
2024-07-23 14:13:46 -0300

    slack team member controller was created

--- api/src/modules/slack_team_member/@types/controller.d.ts
+++ api/src/modules/slack_team_member/@types/controller.d.ts
@@ -4,11 +4,11 @@ import { SlackTeamMemberOutput } from "./entities";
 export interface SlackTeamMemberController {
   list: RequestHandler<
     { slack_team_id: string },
-    void,
     {
       slack_team_members: SlackTeamMemberOutput[],
-      next_page_cursor: string;
+      next_page_cursor?: string;
     },
+    void,
     { page_cursor?: string }
   >
 }
---
+++ api/src/modules/slack_team_member/controller.ts
@@ -0,0 +1,42 @@
+import { SlackTeamMemberController } from "./@types/controller";
+import { SlackTeamMember, SlackTeamMemberListInput, SlackTeamMemberOutput } from "./@types/entities";
+import { SlackTeamMemberService } from "./@types/service";
+import { SlackTeamMemberValidator } from "./validator";
+import * as yup from 'yup';
+
+export class SlackTeamMemberControllerImpl implements SlackTeamMemberController {
+  private readonly slack_team_members: SlackTeamMemberService;
+
+  constructor(slack_team_members: SlackTeamMemberService) {
+    this.slack_team_members = slack_team_members;
+  }
+
+  list: SlackTeamMemberController['list'] = async (req, res) => {
+    const { slack_team_id } = req.params;
+    const { page_cursor } = req.query;
+    const payload: SlackTeamMemberListInput = {
+      slack_team_id,
+      page_cursor,
+    }
+    const validated = await SlackTeamMemberValidator.list.validate(payload, { stripUnknown: true });
+    const {
+      next_page_cursor,
+      slack_team_members
+    } = await this.slack_team_members.list(validated);
+    const response = {
+      next_page_cursor,
+      slack_team_members: await Promise.all(slack_team_members.map(this.serialize))
+    }
+    res.status(200).json(response)
+  }
+
+  private async serialize(slack_team_member: SlackTeamMember): Promise<SlackTeamMemberOutput> {
+    const schema = yup.object().shape({
+      email: yup.string().required(),
+      slack_id: yup.string().required(),
+      name: yup.string().required(),
+      avatar_url: yup.string().url(),
+    });
+    return schema.cast(slack_team_member, { stripUnknown: true });
+  }
+}
