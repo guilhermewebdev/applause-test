ddfcf94033eecba98624e152d8430c660982b4b7
Guilherme Isaías <guilherme.isaias@ferreri.co>
2024-07-23 13:40:00 -0300

    slack team serive was injected on slack team member service

--- api/src/modules/slack_team_member/service.ts
+++ api/src/modules/slack_team_member/service.ts
@@ -1,15 +1,23 @@
+import { SlackTeamService } from "../slack_team/@types/service";
 import { SlackTeamMember, SlackTeamMemberListInput } from "./@types/entities";
 import { SlackApiTeamMemberRepository } from "./@types/repositories/slack_api_team_member_repository";
 import { SlackTeamMemberService } from "./@types/service";
 
 export class SlackTeamMemberServiceImpl implements SlackTeamMemberService {
   private readonly slack_api_team_member_repository: SlackApiTeamMemberRepository;
+  private readonly slack_teams: SlackTeamService;
 
-  constructor(slack_api_team_member_repository: SlackApiTeamMemberRepository) {
+  constructor(
+    slack_api_team_member_repository: SlackApiTeamMemberRepository,
+    slack_teams: SlackTeamService,
+  ) {
+    this.slack_teams = slack_teams;
     this.slack_api_team_member_repository = slack_api_team_member_repository;
   }
 
   async list(payload: SlackTeamMemberListInput): Promise<{ slack_team_members: SlackTeamMember[]; next_page_cursor?: string; }> {
-    return this.slack_api_team_member_repository.list(payload);    
+    const { slack_team_id, page_cursor } = payload;
+    const slack_team = await this.slack_teams.get(slack_team_id);
+    return this.slack_api_team_member_repository.list(slack_team, page_cursor);
   }
 }
